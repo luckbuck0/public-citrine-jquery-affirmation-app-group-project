b125a9115dd847f819dcd3307cc57660c871f8eb
Joshua Engebretson <joshua.engebretson@gmail.com>
2023-03-31 14:33:30 -0500

    Completed part 2 of assignment
    Built out createAffirmtion function that is called upon when the submit button is clicked.
    	-This function grabs the values from AffirmationInput and AuthorInput then placed this information into a table below.
    	-The function then clears out the values for AffirmationInput and AuthorInput so they are ready to be utilized again.

--- script.js
+++ script.js
@@ -8,8 +8,10 @@ function onReady() {
   // add ❄️ to the div with id="Fire-and-Ice
   $('#Ice-button').on('click', FreezeRay) 
 
-  // Function that retrieves values on form submission
-  $('.')
+  // Function that retrieves values from
+  // AffirmationInput AND AuthorInput upon
+  // form submission
+  $('.submit-button').on('click', createAffirmation)
 
 } // End onReady
 
@@ -20,3 +22,29 @@ function FlameThrower() {
 function FreezeRay() {
   $('#Fire-and-Ice').append('❄️') // add ❄️ to the div with id="Fire-and-Ice"
 } // End FreezeRay
+
+function createAffirmation(event) {
+  event.preventDefault();
+  
+  // Retrieve value of "AffirmationInput" and place into variable
+  let Affirmation_Input = $('#AffirmationInput').val()
+  console.log(Affirmation_Input);
+
+  // Retrieve value of "AuthorInput" and place into variable
+  let Author_Input = $('#AuthorInput').val()
+  console.log(Author_Input);
+
+  // Add both Affirmation_Input and Author_Input to 
+  // the table within the id="tBody"
+  $('tBody').append(`
+      <tr>
+        <td>${Affirmation_Input}</td>
+        <td>${Author_Input}</td>
+      </tr>
+    `)
+
+  // Clear out the values from the input lines.
+  $('#AffirmationInput').val('')
+  $('#AuthorInput').val('')
+
+} // End createAffirmation
