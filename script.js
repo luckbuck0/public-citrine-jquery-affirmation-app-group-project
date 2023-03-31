d9d29f8f0c549a3495c944b87867859b8a3c3de8
Joshua Engebretson <joshua.engebretson@gmail.com>
2023-03-31 13:56:31 -0500

    Fire and Ice buttons create emoji on DOM
    Built out functions to create the fire and ice emojis when each respective button is clicked. These are then placed inside of the Div with an id=Fire-and-Ice.

--- script.js
+++ script.js
@@ -2,4 +2,21 @@ $(document).ready(onReady);
 
 function onReady() {
   console.log('We got this.')
-}
+  // add 🔥 to the div with id="Fire-and-Ice"
+  $('#Fire-button').on('click', FlameThrower) 
+
+  // add ❄️ to the div with id="Fire-and-Ice
+  $('#Ice-button').on('click', FreezeRay) 
+
+  // Function that retrieves values on form submission
+  $('.')
+
+} // End onReady
+
+function FlameThrower() {
+  $('#Fire-and-Ice').append('🔥') // add 🔥 to the div with id="Fire-and-Ice"
+} // FlameThrower
+
+function FreezeRay() {
+  $('#Fire-and-Ice').append('❄️') // add ❄️ to the div with id="Fire-and-Ice"
+} // End FreezeRay
